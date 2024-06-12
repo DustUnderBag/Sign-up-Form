3a0bb471b5174a931beceb43b373e981a200a37e
Yinjo <usamary55@gmail.com>
2024-06-12 17:02:36 -0400

    Add input guides under each of inputs for easier usage
    The input guides reveal once user starts interacting
    with the inputs.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("test");
+const allInputs = document.querySelectorAll("input");
+
+allInputs.forEach(input => 
+    input.addEventListener("click", ()=> {
+        input.classList.add('clicked');
+    }
+));
+
+function markClicked(input) {
+    input.classList.add('clicked');
+}
+
+const input_pwd = document.querySelector("input#pwd");
+const input_cfm_pwd = document.querySelector("input#cfm-pwd");
+
